b6e4b5f5c47a539359770146f1806b8c52a9176f
Thomas Pondelek <thomas.pondelek@page-effect.com>
2026-02-03 17:17:31 +0100

    debug header leerzeichen

--- includes/view/ViewComponents.php
+++ includes/view/ViewComponents.php
@@ -1,4 +1,3 @@
-
 <?php
 function get_scroll_button_html(){
   $up_arrow_icon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve">
